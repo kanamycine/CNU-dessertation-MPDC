b246f31845035ca2a60212c56af29498dd7adf71
kanamycine <jinyeong5320@gmail.com>
2020-06-06 00:05:33 +0900

    commit for grass

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
+//
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
